58b4196941d28c122ee2a69d2fe438c1cbfb997d
mickaelsamson <mickaelsamson85@gmail.com>
2026-04-23 08:47:06 +0530

    Delete models directory

--- models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const User = new mongoose.Schema({
- username:String,
- rank:{type:Number,default:1000},
- tier:{type:String,default:"Bronze"}
-});
-
-module.exports = mongoose.model('User',User);
