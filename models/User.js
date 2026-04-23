fd1e98cb11c18128d89ec732df04e617118a4492
mickaelsamson <mickaelsamson85@gmail.com>
2026-04-23 10:19:56 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+
+  firstName: String,
+  lastName: String,
+  username: String, // pseudo
+
   xp: { type: Number, default: 0 }
 });
 
