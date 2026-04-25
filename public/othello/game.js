fb2790905b850e8b61022db1e1665e3dfe29bebd
mickaelsamson <mickaelsamson85@gmail.com>
2026-04-25 10:17:30 +0530

    Update game.js

--- public/othello/game.js
+++ public/othello/game.js
@@ -146,7 +146,7 @@ function resign(){
 
 socket.on("online_users",users=>{
  document.getElementById("players").innerHTML=
-  Object.entries(users).map(([name,data])=>`<div class="player">${name} (${data.elo})</div>`).join("");
+ Object.entries(users).map(([name,data])=>`<div class="player" data-profile-username="${name}">${name} (${data.elo})</div>`).join("");
 });
 
 socket.on("othello_state",data=>{
